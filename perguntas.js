e48441c3db0f285db449d2a95c7ddd933b0c40a0
Marinajpds <00001083568528sp@al.educacao.sp.gov.br>
2024-11-27 21:18:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'Programação',
+    'O que é Payton?',
+    'O Payton é uma linguagem de programação'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'Acapital da França é Paris'
+    )
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em inglês?',
+    'Oi em inglês é HI (RAI)'
+)
